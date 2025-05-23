2585e1f14afe4c268b975c58143c3d0e87db40e3
Alisha Oza <ozaaalisha2304@gmail.com>
2025-05-23 16:00:36 +0530

    migrated with clever_cloud

--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host:' bhzwy3el1yogd6z5n5gy-mysql.services.clever-cloud.com',
+  user: 'ujxyiirz0uxxkp14',
+  password: 'IVHhggAGALaPCrliiWas',
+  database: 'bhzwy3el1yogd6z5n5gy',
+  port: 3306 // or the port Clever Cloud provides
 });
-db.connect((err) => {
+
+db.connect(err => {
   if (err) {
-    console.error("DB connection failed:", err);
-  } else {
-    console.log("MySQL connected successfully.");
+    console.error('DB connection failed:', err);
+    return;
   }
+  console.log('MySQL connected successfully.');
 });
 
 module.exports = db;
